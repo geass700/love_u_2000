269f7bdbeddbdd4d13f257b14280f4e9b64a5c14
nava <geass700.5@gmaail.com>
2024-05-26 16:34:51 +0700

    วิจัย

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:love_u_2000/login/login_page.dart';
+import 'package:love_u_2000/main/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const loginPage(),
+      home: const MainPage(),
     );
   }
 }
